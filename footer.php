80828fbf2744bf6bbab6c65220936b2449426ce3
Saša Drmić <sasadrmic032@gmail.com>
2017-07-30 18:14:10 +0200

    Subscribe form setup

--- footer.php
+++ footer.php
@@ -2,10 +2,13 @@
         <div class="container">
           <div class="row">
             <div class="col-md-6 mb-5">
-              <form action="#">
+              <form action="//facebook.us16.list-manage.com/subscribe/post?u=db45c9c58b7fb4175f575996a&amp;id=799c67e710" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                 <label for="#subscribe-email" class="subscribe-label">Be the first to know when a new item hits the shop or when a new video is uploaded.</label>
-                <input type="email" class="form-control" id="subscribe-email" name="subscribe-email" placeholder="Your Email">
-                <button type="submit" class="button button--dark-blue">Send</button>
+                <input type="email" class="form-control" id="subscribe-email" name="EMAIL" placeholder="Your Email">
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_db45c9c58b7fb4175f575996a_799c67e710" tabindex="-1" value=""></div>
+                
+                <button type="submit" class="button button--dark-blue">Subscribe</button>
               </form>
             </div>
             <div class="col-md-4 text-center">
